4341f9f5eb788be8e2b3a86fa95ce79a3ff35f8d
Vipul Snehadeep Chawathe <V_S_Chawathe@MSN.com>
2023-06-08 14:19:59 +0530

    Discovered feeds showing within side panel

--- root/background.js
+++ root/background.js
@@ -1,22 +1,65 @@
+let newFeeds = [], subscribed = [], discarded = [], myPort = null
+function MyOverlay() {
+    chrome.action.getBadgeText({}).then(badge => {
+        if ("" == badge) chrome.action.setBadgeText({ text: "New" })
+    })
+}
 chrome.webNavigation.onCompleted.addListener(details => {
-    chrome.tabs.query({ discarded: false }).then(completedTabs => {
-        completedTabs = completedTabs.filter(v => v.id == details.tabId
-            && !(v.url?.startsWith("chrome://")
-                || v.url?.startsWith("about:")))
-        if (0 < completedTabs.length)
+    if (details.url.startsWith("about:") || details.url.startsWith("chrome:")
+        || details.url.includes("://chrome.google.com/webstore")) return
+    chrome.tabs.query({ discarded: false }).then(availableTabs => {
+        requiredTabArray = availableTabs.filter(v => v.id == details.tabId)
+        if (0 < requiredTabArray.length)
             chrome.scripting.executeScript({
                 target: { tabId: details.tabId, frameIds: [details.frameId] },
                 func: () => Array.from(document.querySelectorAll(
                     "link[type*='atom+xml'],link[type*='rss+xml']"),
-                    link => link.href)
+                    link => ({ href: link.href, title: link.title }))
             }).then(res => {
                 res.forEach(r => {
                     if (null != r.result && 0 < r.result.length)
-                        chrome.action.getBadgeText({}).then(badge => {
-                            if ("" == badge)
-                                chrome.action.setBadgeText({ text: "New" })
+                        r.result.forEach(feed => {
+                            if (!(newFeeds.some(feedEntry =>
+                                feed.href == feedEntry.href)
+                                || subscribed.some(feedEntry =>
+                                    feed.href == feedEntry.href)
+                                || discarded.some(feedEntry =>
+                                    feed.href == feedEntry.href))) {
+                                newFeeds.unshift(feed)
+                                if (null != myPort) myPort.postMessage({
+                                    title: "new",
+                                    feedURL: feed.href,
+                                    feedTitle: feed.title
+                                })
+                                else MyOverlay()
+                            }
                         })
                 })
             })
     })
+})
+chrome.runtime.onConnect.addListener(port => {
+    if (port.name === "sidepanel") {
+        myPort = port, port.onDisconnect.addListener(() => {
+            myPort = null
+            if (1 > newFeeds.length) chrome.action.setBadgeText({ text: "" })
+        })
+        port.onMessage.addListener(msg => {
+            switch (msg.title) {
+                case "new": MyOverlay(); discarded.unshift(newFeeds.pop())
+                    break
+                case "new feeds":
+                    discarded = newFeeds.concat(discarded)
+                    newFeeds = []
+            }
+        })
+        port.postMessage({
+            title: "discarded",
+            feeds: discarded
+        })
+        if (0 < newFeeds.length) port.postMessage({
+            title: "new feeds",
+            feeds: newFeeds
+        })
+    }
 })
